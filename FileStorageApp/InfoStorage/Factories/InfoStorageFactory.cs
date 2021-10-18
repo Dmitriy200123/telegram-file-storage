f1a2df10b76553029e26f48a6253a5494bbbfb50
CheK539 <chek539camo@gmail.com>
2021-10-18 16:27:23 +0500

    Filestora-9: Created IInfoStorageFactory.cs and InfoStorageFactory.cs in FileStorageApp.Data.InfoStorage project.

---
+++ FileStorageApp/InfoStorage/Factories/InfoStorageFactory.cs
@@ -0,0 +1,18 @@
+﻿using FileStorageApp.Data.InfoStorage.Config;
+using FileStorageApp.Data.InfoStorage.Storages.Chats;
+using FileStorageApp.Data.InfoStorage.Storages.Files;
+using FileStorageApp.Data.InfoStorage.Storages.FileSenders;
+
+namespace FileStorageApp.Data.InfoStorage.Factories
+{
+    internal record InfoStorageFactory(IDataBaseConfig DataBaseConfig) : IInfoStorageFactory
+    {
+        private IDataBaseConfig DataBaseConfig { get; } = DataBaseConfig;
+
+        public IChatStorage CreateChatStorage() => new ChatStorage(DataBaseConfig);
+
+        public IFilesStorage CreateFileStorage() => new FilesStorage(DataBaseConfig);
+
+        public IFileSenderStorage CreateFileSenderStorage() => new FileSenderStorage(DataBaseConfig);
+    }
+}
