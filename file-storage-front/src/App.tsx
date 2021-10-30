7d21e45bc454ebbb119f11a8d6305127f00d377c
Arthur <65594296+InTheDark-A@users.noreply.github.com>
2021-10-30 11:38:06 +0500

    FILESTORA-13 done custom select logic

--- file-storage-front/src/App.tsx
+++ file-storage-front/src/App.tsx
@@ -7,28 +7,43 @@ import File from "./components/File/File";
 import {setupStore} from "./redux/redux-store";
 import FilesMain from "./components/FilesMain/FilesMain";
 import Select from "./components/utils/Inputs/Select";
+import {useForm} from "react-hook-form";
 
-const App:FC = () => {
+const App: FC = () => {
     return (<div className="App">
-      <Switch>
-        <Route path={"/files"} exact component={FilesMain} />
-        <Route path={"/file"} component={File} />
-        <Redirect to={"/files"}/>
-      </Switch>
+        <Switch>
+            <Route path={"/files"} exact component={FilesMain}/>
+            <Route path={"/file"} component={File}/>
+            <Redirect to={"/files"}/>
+        </Switch>
     </div>)
 }
 
 const TestApp = () => {
-    return <Select/>
+    const {register, handleSubmit, formState: {errors, dirtyFields: {}}, setValue, getValues} = useForm();
+
+    const consoleLog = (e: any) => {
+        console.log(e);
+    }
+
+    const onChangeForm = handleSubmit(consoleLog);
+
+    return <form onChange={onChangeForm}>
+        <Select register={register} name={"categories"} onChangeForm={onChangeForm} setValue={setValue}
+                getValues={getValues}/>
+        <Select register={register} name={"categories2"} onChangeForm={onChangeForm} setValue={setValue}
+                getValues={getValues}/>
+    </form>
 }
 
 const store = setupStore();
+
 function FileStorageApp() {
     return (
         <BrowserRouter>
-          <Provider store={store}>
-            <TestApp/>
-          </Provider>
+            <Provider store={store}>
+                <App/>
+            </Provider>
         </BrowserRouter>
     );
 }
--- file-storage-front/src/components/utils/Inputs/Select.tsx
+++ file-storage-front/src/components/utils/Inputs/Select.tsx
@@ -1,27 +1,49 @@
-import React from 'react';
-import {useForm} from "react-hook-form";
+import React, {useState} from 'react';
+import {Category} from "../../../models/File";
+import "./Select.scss";
 
-const Select: React.FC = () => {
-    const options = {};
-    const name = "123";
-    const {register, handleSubmit, watch, formState: {errors}, setValue} = useForm();
-    watch("example")
-    const select = watch("example",'value');
-    const onchange = (e: any) => {
-        console.log(e)
-    }
+const optionsCategory: Array<{ value: Category, label: Category }> = [
+    {value: 'images', label: 'images'},
+    {value: 'links', label: 'links'},
+    {value: 'video', label: 'video'},
+    {value: 'documents', label: 'documents'},
+];
+
+type Props = {name: string, onChangeForm: any, className?:string, register: any, getValues: any, setValue: any }
+
+const Select: React.FC<Props> = ({name, onChangeForm, className, register, getValues, setValue }) => {
+    const [isOpen, changeOpen] = useState(false);
+
+    const ui = optionsCategory.map((elem) => {
+        const onChange = () => {
+            const values = getValues(name);
+            if (values.includes(elem.value)) {
+                setValue(name, values.filter((v: Category) => v !== elem.value));
+            } else {
+                setValue(name, [...values, elem.value])
+            }
+
+            onChangeForm();
+        };
+
+        const values = getValues(name);
+        return <li
+            className={"select__option " + (values && (values.includes(elem.value)) ? "select__option_active" : "")}
+            onClick={onChange}>{elem.label}</li>;
+    })
+
 
     return (
-        <form onChange={handleSubmit(onchange)} onSubmit={handleSubmit(onchange)}>
+        <div className={className}>
             <div style={{padding: 100}} className={"select"}>
-                <div onClick={() => setValue("example",["1"])}>Клик меня</div>
-                <select multiple={true} {...register("example")}>
-                    <option value={1}>1111</option>
-                    <option value={2}>222</option>
-                </select>
+                {getValues(name)}
+                <div className={"select__field"} onClick={() => changeOpen(!isOpen)}>Тыкни сюда</div>
+                <ul className={"select__list " + (isOpen ? "select__list_open" : "")}>
+                    {ui}
+                </ul>
+                <select multiple={true} {...register(name, {onChange: () => onChangeForm})}/>
             </div>
-            <button type={"submit"}>asd</button>
-        </form>
+        </div>
     )
 }
 
