7388ce49a7cafde34a21c8f53e0e658da7c990cb
Ponomarev Ivan <raccoon.twitch@gmail.com>
2021-11-30 17:59:18 +0500

    FILESTORA-38 created app to fill database

---
+++ FileStorageApp/DataBaseFiller/ActionProvider.cs
@@ -0,0 +1,19 @@
+﻿using DataBaseFiller.Actions;
+
+namespace DataBaseFiller
+{
+    public class ActionProvider
+    {
+        private readonly IAction _infoAction = new ShowDataBaseInfoAction();
+        private readonly IAction _addAction = new AddDataBaseInfoAction();
+        public IAction? ChooseAction(string command)
+        {
+            return command switch
+            {
+                "info" => _infoAction,
+                "add" => _addAction,
+                _ => null
+            };
+        }
+    }
+}
