ffff0dd9e110a7f8ef4716b931cbc75df389803e
Dmitry Demin <vik555729@gmail.com>
2021-12-01 21:42:27 +0500

    Delete files from s3 in DataBaseFiller

--- FileStorageApp/DataBaseFiller/DataBaseCleaner.cs
+++ FileStorageApp/DataBaseFiller/DataBaseCleaner.cs
@@ -1,11 +1,12 @@
 ﻿using System.Threading.Tasks;
+using FilesStorage.Interfaces;
 using FileStorageApp.Data.InfoStorage.Factories;
 
 namespace DataBaseFiller
 {
     public static class DataBaseCleaner
     {
-        public static async Task CleanUp(IInfoStorageFactory infoStorageFactory)
+        public static async Task CleanUp(IInfoStorageFactory infoStorageFactory, IFilesStorageFactory filesStorageFactory)
         {
             using var filesStorage = infoStorageFactory.CreateFileStorage();
             using var chatStorage = infoStorageFactory.CreateChatStorage();
@@ -16,6 +17,10 @@ namespace DataBaseFiller
                 await chatStorage.DeleteAsync(element.Id);
             foreach (var element in filesStorage.GetAllAsync().Result)
                 await filesStorage.DeleteAsync(element.Id);
+
+            using var physicalFilesStorage = await filesStorageFactory.CreateAsync();
+            foreach (var file in await physicalFilesStorage.GetFilesAsync())
+                await physicalFilesStorage.DeleteFileAsync(file.Key);
         }
     }
 }
--- FileStorageApp/DataBaseFiller/Program.cs
+++ FileStorageApp/DataBaseFiller/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Amazon.S3;
+using FilesStorage;
+using FilesStorage.Interfaces;
 using FileStorageApp.Data.InfoStorage.Config;
 using FileStorageApp.Data.InfoStorage.Factories;
 using Microsoft.Extensions.Configuration;
@@ -9,7 +12,8 @@ namespace DataBaseFiller
     {
         private static readonly HelpInfoProvider HelpInfoProvider = new();
         private static readonly ActionProvider ActionProvider = new();
-        private static InfoStorageFactory infoStorageFactory;
+        private static InfoStorageFactory _infoStorageFactory;
+        private static IFilesStorageFactory _filesStorageFactory;
 
         private static readonly IConfigurationRoot Config = new ConfigurationBuilder()
             .AddJsonFile("appsettings.test.json")
@@ -22,7 +26,7 @@ namespace DataBaseFiller
             Console.WriteLine("Should clean up data base? If yes enter any symbol or press enter to skip");
             var cleanUp = Console.ReadLine();
             if(cleanUp!.Length != 0)
-                DataBaseCleaner.CleanUp(infoStorageFactory).ConfigureAwait(true);
+                DataBaseCleaner.CleanUp(_infoStorageFactory, _filesStorageFactory).ConfigureAwait(true);
             var helpString = HelpInfoProvider.GetHelpInfo();
             Console.WriteLine(helpString);
             while (true)
@@ -30,7 +34,7 @@ namespace DataBaseFiller
                 var command = Console.ReadLine();
                 var action = ActionProvider.ChooseAction(command!);
                 if (action != null)
-                    action.DoAction(infoStorageFactory);
+                    action.DoAction(_infoStorageFactory);
                 else
                     Console.WriteLine("Unknown command");
             }
@@ -44,7 +48,17 @@ namespace DataBaseFiller
                                             $"Port={Config["DbPort"]};" +
                                             $"Password={Config["DbPassword"]};" +
                                             "SSLMode=Prefer");
-            infoStorageFactory = new InfoStorageFactory(config);
+            _infoStorageFactory = new InfoStorageFactory(config);
+            
+            var s3Config = new AmazonS3Config
+            {
+                ServiceURL = Config["S3serviceUrl"],
+                ForcePathStyle = true
+            };
+            var amazonConfig = new S3FilesStorageOptions(Config["S3accessKey"], Config["S3secretKey"],
+                Config["S3bucketName"], s3Config, S3CannedACL.PublicReadWrite,
+                TimeSpan.FromHours(1));
+            _filesStorageFactory = new S3FilesStorageFactory(amazonConfig);
         }
     }
 }
