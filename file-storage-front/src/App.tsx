38107f778cf863ae19671a2e570d3c7b041c6590
Arthur <65594296+InTheDark-A@users.noreply.github.com>
2021-12-07 17:14:32 +0500

    FILESTORA-45 add routing start-page

--- file-storage-front/src/App.tsx
+++ file-storage-front/src/App.tsx
@@ -15,33 +15,38 @@ const App: FC = () => {
     const {isOpen, id, content} = modalConfirm;
     const Content = modalContents[content || 0];
 
-    return (<div className="App" style={{display:"flex", flexDirection: "column", minHeight:"100vh"}}>
-        <header className="header"/>
-        <div style={{display: "grid", gap: 10}}>
-            <Link to={"/load/"}>Загрузить файл</Link>
-            <Link to={"/files"}>Искать файлы</Link>
-            <Link to={"/login"}>логин</Link>
-        </div>
-        {!!error && <div style={{
-            position: "fixed",
-            right: 0,
-            top: 100,
-            background: "#ff061b",
-            padding: 10,
-            color: "white",
-            borderRadius: 5
-        }}>{error}</div>}
-        {loading && <div>Загрузочка</div>}
-        <div style={{flex:"1 1 auto", display:"flex", flexDirection: "column"}}>
-            <Switch>
-                <Route path={"/files"} exact component={FilesMain}/>
-                <Route path={"/file/:id"} component={OpenedFile}/>
-                <Route path={"/load/"} component={LoadFileMain}/>
-                <Route path={"/login"} component={StartPage}/>
-                <Redirect to={"/files"}/>
-            </Switch>
-        </div>
-        {isOpen && id && <Content id={id} /> }
+    return (<div className="App" style={{display: "flex", flexDirection: "column", minHeight: "100vh"}}>
+        <Switch>
+            <Route path={"/login"} component={StartPage}/>
+            <Route>
+                <header className="header"/>
+                <div style={{display: "grid", gap: 10}}>
+                    <Link to={"/load/"}>Загрузить файл</Link>
+                    <Link to={"/files"}>Искать файлы</Link>
+                    <Link to={"/login"}>логин</Link>
+                </div>
+                {!!error && <div style={{
+                    position: "fixed",
+                    right: 0,
+                    top: 100,
+                    background: "#ff061b",
+                    padding: 10,
+                    color: "white",
+                    borderRadius: 5
+                }}>{error}</div>}
+                {loading && <div>Загрузочка</div>}
+                <div style={{flex: "1 1 auto", display: "flex", flexDirection: "column"}}>
+                    <Switch>
+                        <Route path={"/files"} exact component={FilesMain}/>
+                        <Route path={"/file/:id"} component={OpenedFile}/>
+                        <Route path={"/load/"} component={LoadFileMain}/>
+                        <Route path={"/login"} component={StartPage}/>
+                        <Redirect to={"/files"}/>
+                    </Switch>
+                </div>
+                {isOpen && id && <Content id={id}/>}
+            </Route>
+        </Switch>
     </div>)
 }
 
