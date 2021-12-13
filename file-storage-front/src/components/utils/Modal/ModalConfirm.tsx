5d5b669e8426f9b5d67d4d99dc55e3f3dc237eb3
Arthur <65594296+InTheDark-A@users.noreply.github.com>
2021-12-13 15:40:40 +0500

    FILESTORA-51 added remove redirect

--- file-storage-front/src/components/utils/Modal/ModalConfirm.tsx
+++ file-storage-front/src/components/utils/Modal/ModalConfirm.tsx
@@ -5,12 +5,12 @@ import Modal from "./Modal";
 import {useDispatch} from "react-redux";
 import {filesSlice} from "../../../redux/filesSlice";
 import {fetchRemoveFile} from "../../../redux/fileThunks";
+import {useHistory} from "react-router-dom";
 
 const {closeModal} = filesSlice.actions;
 
-export const ModalConfirm: React.FC<{id:string}> = ({id}) => {
-    // if (id === null)
-    //     return null;
+export const ModalConfirm: React.FC<{ id: string }> = ({id}) => {
+    const history = useHistory();
     const dispatch = useDispatch();
     return (
         <Modal>
@@ -18,8 +18,12 @@ export const ModalConfirm: React.FC<{id:string}> = ({id}) => {
                 <h2 className={"modal-confirm__h2"}>Подтверждение</h2>
                 <p className={"modal-confirm__p"}>Вы действительно хотите удалить это файл?</p>
                 <div className={"modal-confirm__btns"}>
-                    <Button onClick={() => dispatch(fetchRemoveFile(id))}>Да</Button>
-                    <Button onClick={() => dispatch(closeModal())} type={"transparent"} >Нет</Button>
+                    <Button onClick={async () => {
+                        await dispatch(fetchRemoveFile(id));
+                        if (history.location.pathname !== "files")
+                            history.push("/files");
+                    }}>Да</Button>
+                    <Button onClick={() => dispatch(closeModal())} type={"transparent"}>Нет</Button>
                 </div>
                 {id}
             </div>
