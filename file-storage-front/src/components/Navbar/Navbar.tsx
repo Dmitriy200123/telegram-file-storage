86779533f57ed875a24efff6876950624b4a739b
Arthur <65594296+InTheDark-A@users.noreply.github.com>
2021-12-13 22:59:10 +0500

    FILESTORA-51 update UI navbar

--- file-storage-front/src/components/Navbar/Navbar.tsx
+++ file-storage-front/src/components/Navbar/Navbar.tsx
@@ -2,16 +2,18 @@ import "./Navbar.scss";
 import React, {FC, memo} from "react";
 import {Link} from "react-router-dom";
 import {ReactComponent as Logo} from "./../../assets/logo.svg";
+import {ReactComponent as Download} from "./../../assets/download_2.svg";
+import {ReactComponent as Search} from "./../../assets/search.svg";
 
 export const Navbar: FC<{ className?: string }> = memo(({className}) => {
     return (<div className={(`${className} ` ?? "") + "navbar"}>
             <div className={"navbar__logo"}>
                 <Logo/>
             </div>
-            <div style={{display: "grid", gap: 10}}>
-                <Link to={"/load/"}>Загрузить файл</Link>
-                <Link to={"/files"}>Искать файлы</Link>
-                <Link to={"/login"}>логин</Link>
+            <div className={"navbar__links"}>
+                <Link to={"/load/"} className={"navbar__link"}><Download /><span>Загрузка файлов</span></Link>
+                <Link to={"/files"} className={"navbar__link"}><Search/>Поиск файлов</Link>
+                <Link to={"/login"} className={"navbar__link"}>логин</Link>
             </div>
         </div>
     );
