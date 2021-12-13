2d544277cac70400dc7b4f2f5a9beb937261c6b6
Arthur <65594296+InTheDark-A@users.noreply.github.com>
2021-12-14 00:22:52 +0500

    абобус

--- file-storage-front/src/components/Navbar/Navbar.tsx
+++ file-storage-front/src/components/Navbar/Navbar.tsx
@@ -5,15 +5,16 @@ import {ReactComponent as Logo} from "./../../assets/logo.svg";
 import {ReactComponent as Download} from "./../../assets/download_2.svg";
 import {ReactComponent as Search} from "./../../assets/search.svg";
 
+
 export const Navbar: FC<{ className?: string }> = memo(({className}) => {
     return (<div className={(`${className} ` ?? "") + "navbar"}>
             <div className={"navbar__logo"}>
                 <Logo/>
             </div>
             <div className={"navbar__links"}>
-                <Link to={"/load/"} className={"navbar__link"}><Download /><span>Загрузка файлов</span></Link>
+                <Link to={"/load/"} className={"navbar__link"}><Download/><span>Загрузка файлов</span></Link>
                 <Link to={"/files"} className={"navbar__link"}><Search/>Поиск файлов</Link>
-                <Link to={"/login"} className={"navbar__link"}>логин</Link>
+                <Link to={"/login"} className={"navbar__link"}>Выйти из аккаунта</Link>
             </div>
         </div>
     );
--- file-storage-front/src/components/StartPage/StartPage.tsx
+++ file-storage-front/src/components/StartPage/StartPage.tsx
@@ -2,8 +2,10 @@ import "./StartPage.scss";
 import Logo from "./../../assets/logos/grey.png";
 import {Button} from "../utils/Button/Button";
 import {FC, memo} from "react";
+import {useHistory} from "react-router-dom";
 
 export const StartPage: FC = memo(() => {
+    const history = useHistory();
     return (
         <div className={"start-page"}>
             <div>
@@ -13,7 +15,7 @@ export const StartPage: FC = memo(() => {
                 <h1 className={"start-page__title"}>Хранилище файлов</h1>
                 <p className={"start-page__description"}>— Сервис, позволяющий автоматически собирать файлы из чатов
                     Telegram и сохранять в хранилище с информацией о файле.</p>
-                <Button type={"white"} className={"start-page__btn"}>Войти через GitLab →</Button>
+                <Button type={"white"} className={"start-page__btn"} onClick={() => history.push("/")}>Войти через GitLab →</Button>
             </div>
         </div>
     );
