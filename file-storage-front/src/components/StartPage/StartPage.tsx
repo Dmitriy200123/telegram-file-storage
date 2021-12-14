2dc94b151658754e9957cdff2974aecc5781f3b2
Arthur <65594296+InTheDark-A@users.noreply.github.com>
2021-12-14 20:45:13 +0500

    FILESTORA-50 add auth window

--- file-storage-front/src/components/StartPage/StartPage.tsx
+++ file-storage-front/src/components/StartPage/StartPage.tsx
@@ -1,11 +1,22 @@
 import "./StartPage.scss";
 import Logo from "./../../assets/logos/grey.png";
 import {Button} from "../utils/Button/Button";
-import {FC, memo} from "react";
+import {FC, memo, useEffect} from "react";
 import {useHistory} from "react-router-dom";
 
 export const StartPage: FC = memo(() => {
     const history = useHistory();
+    // useWindowFocus(() => checkGitLabAuthorization({ isCheckAuth }))
+    //
+    // useEffect(() => {
+    //     if (!isCheckAuth) {
+    //         checkGitLabAuthorization({ isCheckAuth })
+    //     }
+    // }, [isCheckAuth, checkGitLabAuthorization])
+
+    const openAuthInNewWindow = () => {
+        window.open('https://localhost:5001/auth/gitlab', '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes')
+    }
     return (
         <div className={"start-page"}>
             <div>
@@ -17,7 +28,8 @@ export const StartPage: FC = memo(() => {
                     Telegram и сохранять в хранилище с информацией о файле.</p>
                 <Button type={"white"} className={"start-page__btn"} onClick={async () => {
                     try {
-                        document.location ="https://localhost:5001/auth/gitlab";
+                        openAuthInNewWindow()
+                        // document.location ="https://localhost:5001/auth/gitlab";
                     } catch {
 
                     }
