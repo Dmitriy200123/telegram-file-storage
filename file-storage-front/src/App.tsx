133af69fb5bbf83df4b4cb041b95849102aef239
Arthur <65594296+InTheDark-A@users.noreply.github.com>
2021-12-14 15:02:21 +0500

    FILESTORA-51 add first request auth

--- file-storage-front/src/App.tsx
+++ file-storage-front/src/App.tsx
@@ -1,6 +1,6 @@
 import React, {FC} from 'react';
 import './App.css';
-import {BrowserRouter, Link, Redirect, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Link, Redirect, Route, Switch, useLocation} from "react-router-dom";
 import {Provider} from "react-redux";
 import {setupStore} from "./redux/redux-store";
 import {OpenedFile} from "./components/File/OpenFile";
@@ -13,38 +13,49 @@ import {Messages} from "./components/utils/Messages/Messages";
 import {Navbar} from "./components/Navbar/Navbar";
 import Loading from "./components/utils/Loading/Loading";
 
-const App: FC = () => {
-    const {filesReducer, profile} = useAppSelector((state) => state);
-    const {messages} = profile;
-    const {loading, modalConfirm} = filesReducer;
-    const {isOpen, id, content} = modalConfirm;
-    const Content = modalContents[content || 0];
 
+const App: FC = () => {
+    const {profile} = useAppSelector((state) => state);
+    const {messages} = profile;
+    const location = useLocation();
+    console.log(location);
+    console.log(new URLSearchParams(location.search).get('token'));
+    console.log(new URLSearchParams(location.search).get('refreshToken'));
     return (<div className="App app">
         {!!messages.length && <Messages messages={messages} className={"app__messages"}/>}
-
         <Switch>
             <Route path={"/login"} component={StartPage}/>
             <Route>
-                <Navbar className={"app__navbar"}/>
-                {loading && <Loading/>}
-                <div className={"app__content"}>
-                    <header className="header"/>
-                    <div style={{flex: "1 1 auto", display: "flex", flexDirection: "column"}}>
-                        <Switch>
-                            <Route path={"/files"} exact component={FilesMain}/>
-                            <Route path={"/file/:id"} component={OpenedFile}/>
-                            <Route path={"/load/"} component={LoadFileMain}/>
-                            <Route path={"/login"} component={StartPage}/>
-                            <Redirect to={"/files"}/>
-                        </Switch>
-                    </div>
-                    {isOpen && id && <Content id={id}/>}
-                </div>
+                <Content1 />
             </Route>
         </Switch>
     </div>)
 }
+
+const Content1: FC = () => {
+    const {filesReducer, profile} = useAppSelector((state) => state);
+    const {loading, modalConfirm} = filesReducer;
+    const {isOpen, id, content} = modalConfirm;
+    const Content = modalContents[content || 0];
+    return (<>
+        <Navbar className={"app__navbar"}/>
+        {loading && <Loading/>}
+        <div className={"app__content"}>
+            <header className="header"/>
+            <div style={{flex: "1 1 auto", display: "flex", flexDirection: "column"}}>
+                <Switch>
+                    <Route path={"/files"} exact component={FilesMain}/>
+                    <Route path={"/file/:id"} component={OpenedFile}/>
+                    <Route path={"/load/"} component={LoadFileMain}/>
+                    <Route path={"/login"} component={StartPage}/>
+                    <Redirect to={"/files"}/>
+                </Switch>
+            </div>
+            {isOpen && id && <Content id={id}/>}
+        </div>
+    </>)
+}
+
 
 
 const store = setupStore();
