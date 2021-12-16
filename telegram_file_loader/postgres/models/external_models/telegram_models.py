520d8b4d719e119dd763a984f704475b2a072d87
Nikita Buguev <buguev@tochka.com>
2021-12-16 12:42:20 +0500

    fix url without scheme

--- telegram_file_loader/postgres/models/external_models/telegram_models.py
+++ telegram_file_loader/postgres/models/external_models/telegram_models.py
@@ -1,5 +1,9 @@
-from pydantic import AnyUrl, BaseModel, Field
+from pydantic import AnyUrl, BaseModel, Field, validator
 
 
 class ValidateUrl(BaseModel):
     url: AnyUrl = Field(..., description='Урл для валидации')
+
+    @validator('url', pre=True)
+    def http(cls, v: str):
+        return v if '//' in v else f'https://{v}'
--- telegram_file_loader/tests/test_telegram_models/test_telegram_models.py
+++ telegram_file_loader/tests/test_telegram_models/test_telegram_models.py
@@ -7,11 +7,13 @@ async def test_url():
     url3 = 'http://127.0.0.1'
     url4 = 'http://www.test4.com:5000'
     url5 = 'tcp://www.test5.com:5000'
+    url6 = 'www.test5.com:5000'
 
     url_model1 = ValidateUrl(url=url1)
     url_model2 = ValidateUrl(url=url2)
     url_model3 = ValidateUrl(url=url3)
     url_model4 = ValidateUrl(url=url4)
     url_model5 = ValidateUrl(url=url5)
+    url_model5 = ValidateUrl(url=url6)
 
     print(123)
