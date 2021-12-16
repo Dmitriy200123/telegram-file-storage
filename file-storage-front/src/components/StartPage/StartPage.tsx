a22ad0346f25f12af4c2498954ddb0f3137b8c5e
Arthur <65594296+InTheDark-A@users.noreply.github.com>
2021-12-16 14:50:21 +0500

    FILESTORA-50 auth onclick

--- file-storage-front/src/components/StartPage/StartPage.tsx
+++ file-storage-front/src/components/StartPage/StartPage.tsx
@@ -1,7 +1,7 @@
 import "./StartPage.scss";
 import Logo from "./../../assets/logos/grey.png";
 import {Button} from "../utils/Button/Button";
-import {FC, memo, useEffect} from "react";
+import {FC, memo, useEffect, useState} from "react";
 import {GitlabAuth} from "gitlab-auth";
 import {useHistory} from "react-router-dom";
 import {useAppDispatch} from "../../utils/hooks/reduxHooks";
@@ -9,6 +9,7 @@ import {fetchAuthGitlab} from "../../redux/profileThunks";
 
 export const StartPage: FC = memo(() => {
     const dispatch= useAppDispatch();
+    const [clicked, ChangeClicked] = useState(false);
     useEffect(() => {
         let dataAuthGit = sessionStorage.getItem("oidc.user:https://git.66bit.ru:392b8f8766b8da0f5f64edaa50b89b633d302ab0fd7f94aa482d5510e1a97cda");
         if (dataAuthGit){
@@ -18,13 +19,15 @@ export const StartPage: FC = memo(() => {
     }, [sessionStorage])
     return (
         <div>
-            <GitlabAuth
-                host="https://git.66bit.ru"
-                application_id="392b8f8766b8da0f5f64edaa50b89b633d302ab0fd7f94aa482d5510e1a97cda"
-                redirect_uri="http://localhost:3000/login2"
-                scope="api openid profile email"
-                secret={"3446700f6a9e418bee18bbd32f4df0fc6e2749182faaf3bebbbc3ebd4c5b1325"}
-             />
+            {clicked &&
+                <GitlabAuth
+                    host="https://git.66bit.ru"
+                    application_id="392b8f8766b8da0f5f64edaa50b89b633d302ab0fd7f94aa482d5510e1a97cda"
+                    redirect_uri="http://localhost:3000/login2"
+                    scope="api openid profile email"
+                    secret={"3446700f6a9e418bee18bbd32f4df0fc6e2749182faaf3bebbbc3ebd4c5b1325"}
+                />
+            }
             <div className={"start-page"}>
                  <div>
                      <img src={Logo}/>
@@ -33,7 +36,7 @@ export const StartPage: FC = memo(() => {
                      <h1 className={"start-page__title"}>Хранилище файлов</h1>
                      <p className={"start-page__description"}>— Сервис, позволяющий автоматически собирать файлы из чатов
                          Telegram и сохранять в хранилище с информацией о файле.</p>
-                     <Button type={"white"} className={"start-page__btn"}>Войти через GitLab →</Button>
+                     <Button type={"white"} className={"start-page__btn"} onClick={() => ChangeClicked(true)}>Войти через GitLab →</Button>
                  </div>
              </div>
         </div>
