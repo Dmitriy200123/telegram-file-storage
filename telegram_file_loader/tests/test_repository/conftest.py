45f214c97f566d80ed8d340432aa45fc7cc34b09
CheK539 <chek539camo@gmail.com>
2021-12-20 00:42:17 +0500

    FILESTORA-47: Add TagRepository tests

--- telegram_file_loader/tests/test_repository/conftest.py
+++ telegram_file_loader/tests/test_repository/conftest.py
@@ -1,14 +1,19 @@
 import pytest
-from tests.test_postgres_models.conftest import create_marked_text_tags
+from common.repository.tag_repository import TagRepository
 
 
-@pytest.fixture
-async def init_tags():
-    await create_marked_text_tags()
-
-
-def create_message(title: str, description: str, title_tag: str = None, description_tag: str = None) -> str:
+def create_message(
+    title: str = None,
+    description: str = None,
+    title_tag: str = None,
+    description_tag: str = None
+) -> str:
     title_tag = title_tag or '#'
     description_tag = description_tag or '*'
     return f"{title_tag}{title or 'Title'}{title_tag} some text " \
            f"{description_tag}{description or 'Description'}{description_tag}"
+
+
+@pytest.fixture
+def tag_repository(db_manager):
+    return TagRepository(db_manager)
