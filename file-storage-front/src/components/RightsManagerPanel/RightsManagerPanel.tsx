e161bb40eef6dfd813d61feae2f9f914d019b4e3
Arthur <65594296+InTheDark-A@users.noreply.github.com>
2021-12-23 12:26:18 +0500

    FILESTORA-55 update input  rights

--- file-storage-front/src/components/RightsManagerPanel/RightsManagerPanel.tsx
+++ file-storage-front/src/components/RightsManagerPanel/RightsManagerPanel.tsx
@@ -1,27 +1,19 @@
-import React, {memo} from "react";
+import React, {memo, useState} from "react";
 import "./RightsManagerPanel.scss";
 import {ReactComponent as SearchSvg} from "./../../assets/search.svg";
 import {ReactComponent as Employee} from "./../../assets/add-employee.svg";
 import {ReactComponent as Account} from "./../../assets/account.svg";
 import {RightsModal} from "./RightsModal/RightsModal";
+import {OutsideAlerter} from "../utils/OutSideAlerter/OutSideAlerter";
 
 export const RightsManagerPanel: React.FC = memo(() => {
-    // return <RightsModal/>
     return (
         <div className={"rights-panel"}>
             <h2 className={"rights-panel__h2"}>Добавление доступа</h2>
             <div className={"rights-panel__content"}>
                 <div className={"rights-panel__search-block"}>
                     <h3 className={"rights-panel__h3"}>Сотрудник</h3>
-                    <label className={"rights-panel__search-block-label"}>
-                        <input className={"rights-panel__search-block-input"} placeholder={"Введите имя сотрудника"}/>
-                        <SearchSvg className={"rights-panel__search-block-svg"}/>
-                        <section className={"rights-panel__search-block-dropdown"}>
-                            <div className={"rights-panel__search-block-dropdown-item"}>
-                                <Account/><span>Абобус</span>
-                            </div>
-                        </section>
-                    </label>
+                    <InputDropdown/>
                 </div>
                 <div className={"rights-panel__employee-info"}>
                     <Employee className={"rights-panel__employee-info-svg"}/>
@@ -33,6 +25,25 @@ export const RightsManagerPanel: React.FC = memo(() => {
     );
 })
 
+const InputDropdown = () => {
+    const FoundUsers = [1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17].map(() => {
+        return <div className={"rights-panel__search-block-dropdown-item"}>
+            <Account/><span>Абобус</span>
+        </div>
+    })
+    const [text, changeText] = useState("");
+    const [isOpen, changeOpen] = useState(false);
 
+    return <OutsideAlerter className={"rights-panel__search-block-label"} onOutsideClick={() =>  changeOpen(false)}><label className={"rights-panel__search-block-label"}>
+        <input onClick={() => changeOpen(true)} className={"rights-panel__search-block-input"}
+               placeholder={"Введите имя сотрудника"}/>
+        <SearchSvg className={"rights-panel__search-block-svg"}/>
+        {isOpen && <section onBlur={() => changeOpen(false)} className={"rights-panel__search-block-dropdown"}>
+            {FoundUsers}
+        </section>
+        }
+    </label>
+    </OutsideAlerter>
+}
 
 
