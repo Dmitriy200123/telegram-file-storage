e87e1f43ad2f003e8fa0d619cf4e40ea003124d9
Arthur <65594296+InTheDark-A@users.noreply.github.com>
2022-01-15 14:40:40 +0500

    FILESTORA-61 added filter date

--- file-storage-front/src/components/FilesMain/FilesMain.tsx
+++ file-storage-front/src/components/FilesMain/FilesMain.tsx
@@ -9,18 +9,19 @@ import {configFilters} from "./ConfigFilters";
 import {SubmitHandler, useForm} from "react-hook-form";
 import {Select} from "../utils/Inputs/Select";
 import {fetchFiles, fetchFilters} from "../../redux/thunks/mainThunks";
-import {SelectTime} from "../utils/Inputs/SelectDate";
 import {ReactComponent as Search} from "./../../assets/search.svg";
 import {filesSlice} from "../../redux/filesSlice";
+import {SelectTime} from "../utils/Inputs/SelectDate";
 
 
 let isCurrentPageChanged = false;
 const {changePaginatorPage} = filesSlice.actions;
+
 const FilesMain = () => {
     const state = useAppSelector((state) => state);
     const {filesReducer, profile} = state;
     const {rights} = profile;
-    const {files:filesData,filesNames, chats, senders, paginator, filesTypes} = filesReducer;
+    const {files: filesData, filesNames, chats, senders, paginator, filesTypes} = filesReducer;
     const {currentPage, filesInPage} = paginator;
     const dispatch = useAppDispatch();
     const history = useHistory();
@@ -38,27 +39,40 @@ const FilesMain = () => {
     useEffect(() => {
         isCurrentPageChanged = true;
         onChangeForm();
-    },[currentPage])
-
+    }, [currentPage])
 
     const {optionsName, optionsSender, optionsChat} = configFilters(filesNames, chats, senders);
     const optionsCategory = filesTypes && Object.keys(filesTypes).map((key) => ({label: filesTypes[key], value: key}));
     const {register, handleSubmit, formState: {errors}, setValue, getValues} = useForm();
     const dispatchValuesForm: SubmitHandler<any> = (formData) => {
         AddToUrlQueryParams(history, formData);
-        if (isCurrentPageChanged){
-            dispatch(fetchFiles({skip: currentPage > 0 ? (currentPage - 1) * filesInPage : 0, take: filesInPage, ...formData}));
+        let form = {
+            take: filesInPage,
+            senderIds: formData.senderIds, categories: formData.categories,
+            dateTo: formData.date?.dateTo,
+            dateFrom: formData.date?.dateFrom,
+            chatIds: formData.chatIds,
+        };
+        console.log(form);
+        if (isCurrentPageChanged) {
+            dispatch(fetchFiles({
+                skip: currentPage > 0 ? (currentPage - 1) * filesInPage : 0,
+                ...form
+            }));
         } else {
             if (currentPage !== 1)
                 dispatch(changePaginatorPage(1));
             else
-                dispatch(fetchFiles({skip: 0, take: filesInPage, ...formData}));
+                dispatch(fetchFiles({
+                    skip: 0, ...form
+                }));
         }
 
         isCurrentPageChanged = false;
     };
 
-    const FragmentsFiles = filesData.map((f) => <FragmentFile key={f.fileId} file={f} rights={rights || []} types={filesTypes}/>);
+    const FragmentsFiles = filesData.map((f) => <FragmentFile key={f.fileId} file={f} rights={rights || []}
+                                                              types={filesTypes}/>);
 
     const onChangeForm = handleSubmit(dispatchValuesForm);
     const setValueForm = (name: any, value: any) => {
@@ -67,6 +81,7 @@ const FilesMain = () => {
             shouldDirty: true
         });
     }
+
     return (
         <div className={"files-main"}>
             <h2 className={"files-main__title"}>Файлы</h2>
@@ -80,6 +95,10 @@ const FilesMain = () => {
                     <Select name={"fileName"} className={"files__filter files__filter_select"} register={register}
                             onChangeForm={onChangeForm} setValue={setValueForm}
                             values={getValues("fileName")} options={optionsName} isMulti={false}/>
+                    {/*<Select className="files__filter files__filter_select"*/}
+                    {/*        isMulti={false} name={"date"} onChangeForm={onChangeForm} register={register}*/}
+                    {/*        values={getValues("date")}*/}
+                    {/*        setValue={setValueForm} options={optionsDate}/>*/}
                     <SelectTime name={"date"} className={"files__filter files__filter_select"} register={register}
                                 onChangeForm={onChangeForm} setValue={setValueForm}
                                 values={getValues("date")} placeholder={"Выберите дату"}/>
@@ -89,19 +108,19 @@ const FilesMain = () => {
                     <Select name={"senderIds"} className={"files__filter files__filter_select"} register={register}
                             onChangeForm={onChangeForm} setValue={setValueForm}
                             values={getValues("senderIds")} options={optionsSender} isMulti={true}/>
-                    <div className={"files__filter files__filter_last files__filter_select files__filter_search"} >
+                    <div className={"files__filter files__filter_last files__filter_select files__filter_search"}>
                         <Select name={"chatIds"}
                                 register={register}
                                 onChangeForm={onChangeForm} setValue={setValueForm}
                                 values={getValues("chatIds")} options={optionsChat} isMulti={true}/>
                         <button>
-                            <Search />
+                            <Search/>
                         </button>
                     </div>
                     {FragmentsFiles}
                 </form>
             </div>
-            <Paginator paginator={paginator} />
+            <Paginator paginator={paginator}/>
         </div>
     );
 };
@@ -110,10 +129,20 @@ const FilesMain = () => {
 //#region utils
 const AddToUrlQueryParams = (history: any, values: Object) => {
     const urlParams = {};
+    // debugger
     Object.keys(values).forEach(key => {
         // @ts-ignore
         const value = values[key];
-        if (value) {
+        if (key === "date" && value) {
+            if (value.dateFrom) {
+                // @ts-ignore
+                urlParams["dateFrom"] = value.dateFrom;
+            }
+            if (value.dateTo) {
+                // @ts-ignore
+                urlParams["dateTo"] = value.dateTo;
+            }
+        } else if (value) {
             if (value instanceof Array) {
                 if (value.length > 0) {
                     // @ts-ignore
@@ -136,7 +165,7 @@ const GetQueryParamsFromUrl = (history: any) => {
     const fileName = urlSearchParams.get("fileName");
     const senderId = urlSearchParams.get("senderIds")?.split("&")?.map((e) => e);
     const categories = urlSearchParams.get("categories")?.split("&")?.map((e) => e);
-    const date = urlSearchParams.get("date");
+    const date = {dateFrom: urlSearchParams.get("dateFrom"), dateTo: urlSearchParams.get("dateTo")}
     const chats = urlSearchParams.get("chatIds")?.split("&")?.map((e) => e);
     return {fileName, senderId, categories, date, chats};
 }
