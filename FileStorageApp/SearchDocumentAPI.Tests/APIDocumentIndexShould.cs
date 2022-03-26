1f6b461e958eeccabb1b42f740303bef1d14b911
CheK539 <ivan.lazarev539@gmail.com>
2022-03-26 16:59:58 +0500

    FILESTORA-82: Add SearchDocumentAPI.Tests project

---
+++ FileStorageApp/SearchDocumentAPI.Tests/APIDocumentIndexShould.cs
@@ -0,0 +1,111 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentsIndex;
+using DocumentsIndex.Config;
+using DocumentsIndex.Factories;
+using DocumentsIndex.Model;
+using DocumentsIndex.Pipelines;
+using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using SearchDocumentsAPI;
+
+namespace SearchDocumentAPI.Tests
+{
+    public class APIDocumentIndexShould
+    {
+        private WebApplicationFactory<Startup> _applicationFactory;
+        private IElasticConfig _config;
+        private IDocumentIndexStorage _documentIndexStorage;
+        private const string WordThatContainsDocument = "NEST";
+        private const string DocumentName = "example_one.docx";
+
+        [SetUp]
+        public void Setup()
+        {
+            _applicationFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.UseEnvironment("Debug");
+                });
+            
+            _config = new ElasticConfig("http://localhost:9200", "testindex");
+            var factory = new DocumentIndexFactory(new PipelineCreator());
+            _documentIndexStorage = factory.CreateDocumentIndexStorage(_config);
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            var searchResponse = await _documentIndexStorage.SearchBySubstringAsync(WordThatContainsDocument);
+            foreach (var hit in searchResponse)
+                await _documentIndexStorage.DeleteAsync(hit);
+        }
+
+        [Test]
+        public async Task IndexDocument_ReturnOK_ThenCalled()
+        {
+            const HttpStatusCode expected = HttpStatusCode.OK;
+
+            var id = Guid.NewGuid();
+            var bytes = await ReadBytesFromFileName(DocumentName);
+            var document = new Document(id, bytes, DocumentName);
+
+            var json = JsonConvert.SerializeObject(document);
+            var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            using var apiClient = CreateHttpClient();
+            var response = await apiClient.PostAsync($"/api/documentIndex", stringContent);
+
+            response.EnsureSuccessStatusCode();
+            var actual = response.StatusCode;
+
+            actual.Should().Be(expected);
+        }
+        
+        [Test]
+        public async Task DeleteDocumentById_SuccessfullyDelete_ThenCalled()
+        {
+            const HttpStatusCode expected = HttpStatusCode.OK;
+            
+            var id = Guid.NewGuid();
+            var bytes = await ReadBytesFromFileName(DocumentName);
+            var document = new Document(id, bytes, DocumentName);
+            await _documentIndexStorage.IndexDocumentAsync(document);
+            
+            using var apiClient = CreateHttpClient();
+            var response = await apiClient.DeleteAsync($"/api/documentIndex/{id}");
+
+            response.EnsureSuccessStatusCode();
+            var actual = response.StatusCode;
+
+            actual.Should().Be(expected);
+        }
+        
+        private HttpClient CreateHttpClient()
+        {
+            var client = _applicationFactory.CreateClient();
+            client.DefaultRequestHeaders
+                .Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+        
+        private static async Task<byte[]> ReadBytesFromFileName(string fileName)
+        {
+            var stream = File.OpenRead($"{Directory.GetCurrentDirectory()}/FilesForTest/{fileName}");
+            var memoryStream = new MemoryStream();
+            await stream.CopyToAsync(memoryStream);
+            return memoryStream.ToArray();
+        }
+        
+        
+    }
+}
