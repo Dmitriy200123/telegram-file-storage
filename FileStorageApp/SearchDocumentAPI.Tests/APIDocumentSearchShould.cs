47334afa9f4def8a2bb09c2606994566973a42c5
CheK539 <ivan.lazarev539@gmail.com>
2022-04-01 01:03:42 +0500

    FILESTORA-82: Add tests in APIDocumentSearchShould

---
+++ FileStorageApp/SearchDocumentAPI.Tests/APIDocumentSearchShould.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentsIndex;
+using DocumentsIndex.Config;
+using DocumentsIndex.Contracts;
+using DocumentsIndex.Factories;
+using DocumentsIndex.Pipelines;
+using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using SearchDocumentsAPI;
+
+namespace SearchDocumentAPI.Tests
+{
+    public class APIDocumentSearchShould
+    {
+        private WebApplicationFactory<Startup> _applicationFactory;
+        private IElasticConfig _config;
+        private IDocumentIndexStorage _documentIndexStorage;
+
+        [SetUp]
+        public void Setup()
+        {
+            _applicationFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder => { builder.UseEnvironment("Debug"); });
+
+            _config = new ElasticConfig("http://localhost:9200", "testindex");
+            var factory = new DocumentIndexFactory(new PipelineCreator());
+            _documentIndexStorage = factory.CreateDocumentIndexStorage(_config);
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            var searchResponse = await _documentIndexStorage.SearchBySubstringAsync("");
+            foreach (var hit in searchResponse)
+                await _documentIndexStorage.DeleteAsync(hit);
+        }
+
+
+        [TestCaseSource(typeof(APIDocumentSearchData), nameof(APIDocumentSearchData.NameDocumentsSource))]
+        [TestCaseSource(typeof(APIDocumentSearchData), nameof(APIDocumentSearchData.ContentDocumentsSource))]
+        [TestCaseSource(typeof(APIDocumentSearchData), nameof(APIDocumentSearchData.ContentOrNameDocumentsSource))]
+        [TestCaseSource(typeof(APIDocumentSearchData), nameof(APIDocumentSearchData.EmptyDocumentsSource))]
+        public async Task SearchDocuments_OK_ThenCalled(string query, Document[] documents, Guid[] expected)
+        {
+            foreach (var document in documents)
+                await _documentIndexStorage.IndexDocumentAsync(document);
+
+
+            using var apiClient = CreateHttpClient();
+            var response = await apiClient.GetAsync($"api/documents/search?query={query}");
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var actual = JsonConvert.DeserializeObject<Guid[]>(content);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestCaseSource(typeof(APIDocumentSearchData), nameof(APIDocumentSearchData.TextsContainsInDocumentNameSource))]
+        public async Task TextsContainsInDocumentName_OK_ThenCalled(Document document, string[] texts, bool expected)
+        {
+            await _documentIndexStorage.IndexDocumentAsync(document);
+            
+            var json = JsonConvert.SerializeObject(texts);
+            var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+            
+            using var apiClient = CreateHttpClient();
+            var response = await apiClient.PostAsync($"api/documents/{document.Id}/containsInName", stringContent);
+            
+            response.EnsureSuccessStatusCode();
+            
+            var content = await response.Content.ReadAsStringAsync();
+            var actual = JsonConvert.DeserializeObject<bool>(content);
+
+            actual.Should().Be(expected);
+        }
+        
+        private HttpClient CreateHttpClient()
+        {
+            var client = _applicationFactory.CreateClient();
+            client.DefaultRequestHeaders
+                .Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+    }
+}
---
+++ FileStorageApp/SearchDocumentAPI.Tests/Data/APIDocumentSearchData.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Text;
+using DocumentsIndex.Contracts;
+
+namespace SearchDocumentAPI.Tests
+{
+    public static class APIDocumentSearchData
+    {
+        private static readonly Guid FirstDocumentId = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa1");
+        private static readonly Guid SecondDocumentId = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa2");
+
+        public static object[] NameDocumentsSource =
+        {
+            new object[]
+            {
+                "example",
+                GetEmptyDocuments("example_one.docx", "example_two.docx"),
+                new[] {FirstDocumentId, SecondDocumentId}
+            },
+            new object[]
+            {
+                "doc",
+                GetEmptyDocuments("example_one.docx", "example_two.png"),
+                new[] {FirstDocumentId}
+            },
+            new object[]
+            {
+                "two png",
+                GetEmptyDocuments("example_one.docx", "example_two.png"),
+                new[] {SecondDocumentId}
+            }
+        };
+
+        public static object[] ContentDocumentsSource =
+        {
+            new object[]
+            {
+                "hate nest",
+                GetContentDocuments("example_one.docx", "NEST search", "example_two.docx", "Hate this"),
+                new[] {FirstDocumentId, SecondDocumentId}
+            },
+            new object[]
+            {
+                "search",
+                GetContentDocuments("example_one.docx", "NEST search", "example_two.docx", "Hate this"),
+                new[] {FirstDocumentId}
+            },
+            new object[]
+            {
+                "this",
+                GetContentDocuments("example_one.docx", "NEST search", "example_two.docx", "Hate this"),
+                new[] {SecondDocumentId}
+            }
+        };
+        
+        public static object[] ContentOrNameDocumentsSource =
+        {
+            new object[]
+            {
+                "magic example",
+                GetContentDocuments("example.docx", "NEST", "file.txt", "Magic hole"),
+                new[] {FirstDocumentId, SecondDocumentId}
+            },
+            new object[]
+            {
+                "search hole",
+                GetContentDocuments("example.docx", "NEST search", "file.txt", "Magic hole"),
+                new[] {SecondDocumentId, FirstDocumentId}
+            }
+        };
+        
+        public static object[] EmptyDocumentsSource =
+        {
+            new object[]
+            {
+                "magic",
+                GetContentDocuments("example.docx", "NEST", "file.txt", "PAIN"),
+                Array.Empty<Guid>()
+            },
+            new object[]
+            {
+                "peace",
+                GetContentDocuments("example.docx", "Elastic", "file.txt", "hole"),
+                Array.Empty<Guid>()
+            }
+        };
+        
+        public static object[] TextsContainsInDocumentNameSource =
+        {
+            new object[]
+            {
+                new Document(FirstDocumentId, Array.Empty<byte>(), "filename.txt"),
+                new[] {"name"},
+                true
+            },
+            new object[]
+            {
+                new Document(FirstDocumentId, Array.Empty<byte>(), "filename.txt"),
+                new[] {"empty", "name", "test"},
+                true
+            },
+            new object[]
+            {
+                new Document(FirstDocumentId, Array.Empty<byte>(), "foo.txt"),
+                new[] {"empty", "name", "txt"},
+                true
+            },
+            new object[]
+            {
+                new Document(FirstDocumentId, Array.Empty<byte>(), "pain.docx"),
+                new[] {""},
+                true
+            },
+            new object[]
+            {
+                new Document(FirstDocumentId, Array.Empty<byte>(), "pain.docx"),
+                new[] {"p"},
+                true
+            },
+            new object[]
+            {
+                new Document(FirstDocumentId, Array.Empty<byte>(), "pain.docx"),
+                new[] {"pa"},
+                true
+            },
+            new object[]
+            {
+                new Document(FirstDocumentId, Array.Empty<byte>(), "pain.docx"),
+                new[] {"empty", "name", "test"},
+                false
+            },
+            new object[]
+            {
+                new Document(FirstDocumentId, Array.Empty<byte>(), "pain.docx"),
+                new[] {"v"},
+                false
+            },
+            new object[]
+            {
+                new Document(FirstDocumentId, Array.Empty<byte>(), "pain.docx"),
+                new[] {"la", "vo"},
+                false
+            }
+        };
+
+        private static Document[] GetEmptyDocuments(string firstName, string secondName)
+        {
+            return new[]
+            {
+                new Document(FirstDocumentId, Array.Empty<byte>(), firstName),
+                new Document(SecondDocumentId, Array.Empty<byte>(), secondName)
+            };
+        }
+
+        private static Document[] GetContentDocuments(
+            string firstName,
+            string firstContent,
+            string secondName,
+            string secondContent
+        )
+        {
+            return new[]
+            {
+                new Document(FirstDocumentId, Encoding.UTF8.GetBytes(firstContent), firstName),
+                new Document(SecondDocumentId, Encoding.UTF8.GetBytes(secondContent), secondName)
+            };
+        }
+    }
+}
