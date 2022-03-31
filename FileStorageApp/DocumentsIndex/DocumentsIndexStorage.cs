d7503ed87e7fe0c848bd465c5d794947630f986b
CheK539 <ivan.lazarev539@gmail.com>
2022-04-01 00:59:09 +0500

    FILESTORA-82: Move Document model to DocumentsIndex.Contracts.csproj

--- FileStorageApp/DocumentsIndex/DocumentsIndexStorage.cs
+++ FileStorageApp/DocumentsIndex/DocumentsIndexStorage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DocumentsIndex.Contracts;
 using DocumentsIndex.Model;
 using Elasticsearch.Net;
 using Nest;
--- FileStorageApp/DocumentsIndex/IDocumentIndexStorage.cs
+++ FileStorageApp/DocumentsIndex/IDocumentIndexStorage.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using DocumentsIndex.Model;
+using DocumentsIndex.Contracts;
 
 namespace DocumentsIndex
 {
