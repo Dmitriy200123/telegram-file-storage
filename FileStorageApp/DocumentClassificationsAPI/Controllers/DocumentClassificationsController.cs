4472f040d37c0ff10de482a9986bcba9a2183b45
Dmitry Demin <vik555729@gmail.com>
2022-04-24 15:46:25 +0500

    Remove authorize in documentsClassificationController

--- FileStorageApp/DocumentClassificationsAPI/Controllers/DocumentClassificationsController.cs
+++ FileStorageApp/DocumentClassificationsAPI/Controllers/DocumentClassificationsController.cs
@@ -5,7 +5,6 @@ using System.Net;
 using System.Threading.Tasks;
 using DocumentClassificationsAPI.Models;
 using DocumentClassificationsAPI.Services;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
@@ -18,7 +17,6 @@ namespace DocumentClassificationsAPI.Controllers
     [Route("api/documentClassifications")]
     [SwaggerTag("Классификации документов")]
     [ApiController]
-    [Authorize]
     public class DocumentClassificationsController : ControllerBase
     {
         private readonly IDocumentClassificationsService _classificationsService;
